b5a7c87f7862ab2f79a81f1b6d652922fb1ee494
YasiruIsuranga <yasiruisuranga1@gmail.com>
2024-11-10 01:39:24 +0530

    send pr test

--- src/components/buttons/ButtonPrimary.jsx
+++ src/components/buttons/ButtonPrimary.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import "bootstrap";
 
 const ButtonPrimary = ({ onClick, children }) => {
     return (
+        <>
+        <div className='d-flex'>
+        <button className="button-danger" onClick={onClick}>
+            {children}
+            {/* comment 1 */}
+            {/* coomwnt 2 */}
+        </button>
         <button className="button-primary" onClick={onClick}>
             {children}
         </button>
+        </div>
+        </>
     );
 };
 
